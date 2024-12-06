5791288c8ef0869b126c29432922e4dc8172873d
altcomvis <allanlteixeira@gmail.com>
2024-12-06 13:35:22 -0300

    Changes: new button CTA

--- src/components/section-call-to-action.tsx
+++ src/components/section-call-to-action.tsx
@@ -10,16 +10,23 @@ export function SectionCallToAction() {
   }
   return (
     <div className="bg-bg1 bg-cover bg-center bg-no-repeat py-24 flex flex-col items-center gap-6">
-      <span className="text-white text-2xl font-roboto">
-        Aproveite esta oportunidade
-      </span>
-      <Button
-        onClick={handleButtonClick}
-        className="text-2xl p-8 font-roboto"
-        variant="outline"
-      >
-        POR APENAS R$ 147
-      </Button>
+      <div className="w-10/12 flex flex-col items-center justify-center gap-6">
+        <Button
+          onClick={handleButtonClick}
+          className="text-2xl font-roboto uppercase whitespace-normal h-auto"
+          variant="outline"
+        >
+          Aproveite esta oportunidade por apenas R$ 147
+        </Button>
+        <span className="font-roboto text-3xl font-bold text-white">OU</span>
+        <Button
+          onClick={handleButtonClick}
+          className="text-2xl font-roboto uppercase whitespace-normal h-auto"
+          variant="outline"
+        >
+          abra uma conta na genial e invista no tesouro para ter acesso gratuito
+        </Button>
+      </div>
     </div>
   )
 }
